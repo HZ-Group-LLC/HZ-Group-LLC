7570c9c57d1877756ce4102d822496db12d40e17
TahaAbidi110 <taha.razah110@gmail.com>
2026-01-08 09:18:20 -0700

    google code updated layout.js

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export const metadata = {
     "Corporate Trainings",
   ],
   verification: {
-    google: "google6ede5e98a4318505.html",
+    google: "N1JeJ7ujxYDZQOEy0jq3ZIixvG33wkbEwSZCQNhsobY",
   },
   authors: [{ name: "HZ Group LLC" }],
   openGraph: {
